66cab0982d940e073001fc218f5547c0780bb916
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-03-09 20:29:39 +0100

    changed the name of the files

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-	document
-	  .querySelector(".navbar-toggler")
-	  .addEventListener("click", function () {
-		var navbar = document.querySelector(".navbar");
-		var toggleMenu = document.querySelector(".navbar-toggler");
-		var header = document.querySelector(".header");
-  
-		// Check if the navbar is expanded or collapsed
-		const isExpanded = toggleMenu.getAttribute("aria-expanded") == "true";
-		if (isExpanded) {
-		  header.style.marginTop = "190px";
-		} else {
-		  header.style.marginTop = "105px";
-		}
-	  });
-  });
-  
-  
+  document
+    .querySelector(".navbar-toggler")
+    .addEventListener("click", function () {
+      var navbar = document.querySelector(".navbar");
+      var toggleMenu = document.querySelector(".navbar-toggler");
+      var header = document.querySelector("#header");
+
+      // Check if the navbar is expanded or collapsed
+      const isExpanded = toggleMenu.getAttribute("aria-expanded") == "true";
+      if (isExpanded) {
+        header.style.marginTop = "200px";
+      } else {
+        header.style.marginTop = "120px";
+      }
+    });
+});
