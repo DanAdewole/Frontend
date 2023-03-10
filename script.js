1214bc25fcbacbff3416d82224897513ceacfe22
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-03-10 23:13:01 +0100

    toggle button icon to change when active

--- script.js
+++ script.js
@@ -15,3 +15,39 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 });
+
+const navbarToggleBtn = document.querySelector('#navbarToggleBtn');
+
+navbarToggleBtn.addEventListener('click', function () {
+  navbarToggleBtn.classList.toggle('active');
+  if (navbarToggleBtn.classList.contains('active')) {
+    navbarToggleBtn.innerHTML = '<i class="fas fa-times icon-toggle"></i>';
+  } else {
+    navbarToggleBtn.innerHTML = '<span class="navbar-toggler-icon"></span>';
+  }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const selectDrop = document.querySelector('#countries');
+  // const selectDrop = document.getElementById('countries');
+
+
+  fetch('http://restcountries.eu/rest/v2/all').then(res => {
+    return res.json();
+  }).then(data => {
+    let output = "";
+    data.forEach(country => {
+      output += `
+      
+      <option value="${country.name}">${country.name}</option>`;
+    })
+
+    selectDrop.innerHTML = output;
+  }).catch(err => {
+    console.log(err);
+  })
+
+
+});
