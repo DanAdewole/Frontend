8e192f31006331233e985fd3a5b06f8cbdbf9b00
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-03-17 19:57:38 +0100

    add signup form validation

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+// change navbar toggle icon when expanded
 const navbarToggleBtn = document.querySelector("#navbarToggleBtn");
 
 navbarToggleBtn.addEventListener("click", function () {
@@ -55,4 +57,84 @@ if (togglePassword2) {
     this.querySelector("i").classList.toggle("fa-eye");
     this.querySelector("i").classList.toggle("fa-eye-slash");
   });
+}
+
+// signup form validation
+const firstNameInput = document.querySelector("#inputFirstName");
+const lastNameInput = document.querySelector("#inputLastName");
+const emailInput = document.querySelector("#inputEmail");
+const fullNameInput = document.querySelector("#inputFullName");
+const passwordInput = document.querySelector("#password");
+const confirmPasswordInput = document.querySelector("#confirm-password");
+const signupButton = document.querySelector("#sign-up");
+
+// get media query
+const mediaQuery = window.matchMedia("(max-width: 576px)");
+
+if (mediaQuery.matches) {
+  console.log("media query matches");
+  if (fullNameInput) {
+    fullNameInput.addEventListener("input", validateSecondForm);
+  }
+  if (emailInput) {
+    emailInput.addEventListener("input", validateSecondForm);
+  }
+  if (passwordInput) {
+    passwordInput.addEventListener("input", validateSecondForm);
+  }
+} else {
+  // validate form
+  if (firstNameInput) {
+    firstNameInput.addEventListener("input", validateForm);
+  }
+  if (lastNameInput) {
+    lastNameInput.addEventListener("input", validateForm);
+  }
+  if (emailInput) {
+    emailInput.addEventListener("input", validateForm);
+  }
+  if (passwordInput) {
+    passwordInput.addEventListener("input", validatePasswords);
+  }
+  if (confirmPasswordInput) {
+    confirmPasswordInput.addEventListener("input", validatePasswords);
+  }
+}
+
+signupButton.disabled = true;
+
+function validateForm() {
+  if (
+    firstNameInput.value === "" ||
+    lastNameInput.value === "" ||
+    emailInput.value === ""
+  ) {
+    signupButton.disabled = true;
+  } else {
+    validatePasswords();
+  }
+}
+
+function validatePasswords() {
+  if (
+    passwordInput.value !== confirmPasswordInput.value ||
+    passwordInput.value === "" ||
+    confirmPasswordInput.value === ""
+  ) {
+    signupButton.disabled = true;
+  } else {
+    signupButton.disabled = false;
+  }
+}
+
+function validateSecondForm() {
+  if (
+    fullNameInput.value === "" ||
+    emailInput.value === "" ||
+    passwordInput.value === ""
+  ) {
+    signupButton.disabled = true;
+  } else {
+    signupButton.disabled = false;
+  }
 }
